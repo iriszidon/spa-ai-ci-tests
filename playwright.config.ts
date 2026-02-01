ccaf4179f4b9d7c6d79f5372053f00f99abae940
iriso <iris.opper@finonex.com>
2026-02-01 12:06:11 +0200

    Update: Run tests in slow mode

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,8 +25,8 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    /* Slow down Playwright actions by 750ms to make runs easier to observe */
+    launchOptions: { slowMo: 750 },
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
